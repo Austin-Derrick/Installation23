8ccfd1aea6f8711a6c4fd0c26e0c4c3deff63350
Austin-Derrick <aderrickson@colum.edu>
2020-10-02 11:56:08 -0500

    Shooting Mechanic
    Basic shooting mechanic. Player can shoot bullets after picking up the diamond object. Shooting only goes to the right but will be fixed with later updates.

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    Transform anchor;
+    [SerializeField] ShootBullet shootBulletScript;
+
+    private void Start()
+    {
+        anchor = transform.GetChild(2);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Pickup")
+        {
+            collision.transform.position = anchor.position;
+            collision.gameObject.transform.SetParent(anchor);
+
+            shootBulletScript.setIsBeingHeld();
+        }
+    }
+}
---
+++ Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    bool isBeingHeld = false;
+    int ammo = 5;
+    [SerializeField] GameObject bullet;
+    Rigidbody2D bulletRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRB = bullet.GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBeingHeld && Input.GetKeyDown(KeyCode.C))
+        {
+            Transform bulletToShoot = Instantiate(bullet.transform, GameObject.Find("Anchor").transform);
+            bulletToShoot.transform.SetParent(null);
+            Rigidbody2D bulletToShootRB = bulletToShoot.GetComponent<Rigidbody2D>();
+            bulletToShootRB.AddForce(Vector2.right * 50, ForceMode2D.Impulse);
+        }
+    }
+
+    public void setIsBeingHeld()
+    {
+        isBeingHeld = true;
+    }
+}
