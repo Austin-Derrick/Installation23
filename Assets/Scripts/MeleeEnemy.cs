edfa8949c9903c416fc79f9c82bfeeb4e7fabf72
JerrellN <nolen.jerrell@hotmail.com>
2020-10-06 14:10:52 -0500

    Add Basic melee enemy script

---
+++ Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody enemyRb;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector2 lookDirection = (player.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed /** Time.deltaTime*/);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with player, destroy it
+        if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
