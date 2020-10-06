f75e5ea0090f53c3e41e0da7551adcac836f0780
JerrellN <nolen.jerrell@hotmail.com>
2020-10-06 17:50:43 -0500

    Base Setup for melee enemy done
    Need to set up patrol for dormant enemy

--- Assets/Scripts/MeleeEnemy.cs
+++ Assets/Scripts/MeleeEnemy.cs
@@ -4,34 +4,100 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
+    [Header("Attributes")]
     public float speed;
-    private Rigidbody enemyRb;
+    private Rigidbody2D enemyRb;
     private GameObject player;
+    public float MaxHealth = 100f;
+    public float CurrentHealth = 100f;
+    public float damageBounceBack = 5;
+
+    [Space]
+    [Header("Jump Variables")]
+    public float jumpHeight;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private bool grounded;
+    public float checkRadius;
+
+    [Space]
+    [Header("AI")]
+    public bool goodToAttack = true;
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
+        enemyRb = GetComponent<Rigidbody2D>();
         player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Set enemy direction towards player goal and move there
-        Vector2 lookDirection = (player.transform.position - transform.position).normalized;
-        enemyRb.AddForce(lookDirection * speed /** Time.deltaTime*/);
-
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        // If enemy collides with player, destroy it
-        if (other.gameObject.name == "Player Goal")
+        int nonPlayer = 1 << 9;
+        nonPlayer = ~nonPlayer;
+        RaycastHit2D hit;
+        if(CurrentHealth <= 0)
         {
             Destroy(gameObject);
         }
 
+        //Checks to see if the player is within range of the enemy AI
+        hit = Physics2D.Raycast(transform.position, player.transform.position, nonPlayer);
+        if (hit && goodToAttack)
+        {
+            ChaseThePlayer();
+        }
+
+        
+        
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with bullet, destroy it
+        if (other.gameObject.name == "Bullet")
+        {
+            if (transform.position.x > other.gameObject.transform.position.x)
+                enemyRb.velocity = Vector2.left * damageBounceBack;
+            else
+                enemyRb.velocity = Vector2.right * damageBounceBack;
+            CurrentHealth -= 25;
+        }
+        // If Enemy collides with Player, they bounce back in the opposite direction of the player and then 
+        if (other.gameObject.CompareTag("Player"))
+        {
+            goodToAttack = false;
+            if (transform.position.x > other.gameObject.transform.position.x)
+                enemyRb.velocity = Vector2.left * damageBounceBack;
+            else
+                enemyRb.velocity = Vector2.right * damageBounceBack;
+            StartCoroutine(WaitForAttack());
+        }
+
+    }
+
+    private void ChaseThePlayer()
+    {
+        // Set enemy direction towards player and move there
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        if (player.transform.position.y > transform.position.y + 1 && grounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        enemyRb.velocity = Vector2.up * jumpHeight;
+    }
+
+    public IEnumerator WaitForAttack()
+    {
+        yield return new WaitForSeconds(1.5f);
+        goodToAttack = true;
+    }
 }
