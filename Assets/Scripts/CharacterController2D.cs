9c46d073c8bd332d02d707d972dc980500a05e88
Austin-Derrick <aderrickson@colum.edu>
2020-10-13 14:08:35 -0500

    New Branch commit

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -12,18 +12,23 @@ public class CharacterController2D : MonoBehaviour
     float maxSpeed = 20;
 
     new BoxCollider2D  collider;
+    [SerializeField]
+    Camera playerCam;
+
 
     Rigidbody2D rb;
 
     Vector2 input;
-
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask groundLayer;
-
+    Vector3 cameraOffset = new Vector3(0, 0, -10);
     private bool grounded;
 
     private bool isFacingRight = true;
+
+    Vector3 mousePos = new Vector3();
+    Vector3 charPos;
 
     private void Awake()
     {
@@ -43,6 +48,24 @@ public class CharacterController2D : MonoBehaviour
 
     private void Update()
     {
+
+        //Updates mouse and character positions
+        mouseAndCharacterPosition();
+
+        playerCam.transform.position = gameObject.transform.position + cameraOffset;
+        //Character flipping based on mouse position
+        if(mousePos.x >= charPos.x && !isFacingRight)
+        {
+            FlipSprite();
+            isFacingRight = true;
+        }
+        if(mousePos.x <= charPos.x && isFacingRight)
+        {
+            FlipSprite();
+            isFacingRight = false;
+        }
+
+
         input.x = Input.GetAxisRaw("Horizontal");
         if (!isFacingRight && input.x > 0)
         {
@@ -58,6 +81,15 @@ public class CharacterController2D : MonoBehaviour
             rb.velocity = Vector2.up * jumpHeight;
 
         }
+    }
+
+
+    //Uses Vector 3 to acquire character position and mouse position 
+    //relative to the world coordinates using the player camera.
+    private void mouseAndCharacterPosition()
+    {
+        charPos = gameObject.transform.localPosition;
+        mousePos = playerCam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FlipSprite()
---
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    int health;
+
+
+
+
+    private void Die()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void decreaseHealth(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+}
