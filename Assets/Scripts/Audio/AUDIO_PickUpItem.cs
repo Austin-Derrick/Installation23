6342c980d2f577e9218e2f3e7449cc2bd87a1df8
GD278 <57197845+GD278@users.noreply.github.com>
2020-11-05 08:27:43 -0600

    Added Pick Up Sound to Rifle
    Added Pick up Sound/Script to rifle.

--- Assets/Scripts/Audio/AUDIO_PickUpItem.cs
+++ Assets/Scripts/Audio/AUDIO_PickUpItem.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class AUDIO_PickUpItem : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Source and Audioclips
+    AudioSource source;
+    [SerializeField]
+    AudioClip clip;
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            source.clip = clip;
+            source.Play();
+        }
     }
 }
