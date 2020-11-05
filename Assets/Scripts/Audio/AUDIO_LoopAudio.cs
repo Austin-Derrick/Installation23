529f4a50da2e8196e5c9755369dbdb14902c201b
GD278 <57197845+GD278@users.noreply.github.com>
2020-11-05 08:49:21 -0600

    Edited Loop Audio Script and PlayOneShot Script
    I programmed the audio in Loop Audio SCript and PlayOneShot Script

--- Assets/Scripts/Audio/AUDIO_LoopAudio.cs
+++ Assets/Scripts/Audio/AUDIO_LoopAudio.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class AUDIO_LoopAudio : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    //AudioSource and AudioClip
+    AudioSource source;
+    [SerializeField]
+    AudioClip clip; 
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        source.clip = clip;
+        source.loop = true;
+        source.Play();
     }
 }
--- Assets/Scripts/Audio/Audio_PlayOneShot.cs
+++ Assets/Scripts/Audio/Audio_PlayOneShot.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Audio_PlayOneShot : MonoBehaviour
 {
     // Start is called before the first frame update
+    //Source and Audio Source
+    AudioSource source;
+    [SerializeField]
+    AudioClip [] gunshot;
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0) && !source.isPlaying)
+        {
+            source.clip = gunshot[Random.Range(0, gunshot.Length + 1)];
+            source.Play();
+
+        }
     }
 }
