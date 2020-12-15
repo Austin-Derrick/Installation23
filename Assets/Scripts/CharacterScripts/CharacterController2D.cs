8611a41c7769e9734f1caa108c8e23ad908eea8e
JerrellN <nolen.jerrell@hotmail.com>
2020-12-15 14:46:56 -0600

    Revert "Merge branch 'camera-feel'"
    This reverts commit dc25cd55eb780a0c1925fd9282ff85a9f80bfe9d, reversing
    changes made to 3c84e940f8cff087e6ff3cf697f2b1d5440e6628.

--- Assets/Scripts/CharacterScripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterScripts/CharacterController2D.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour
 {
+    #region Component Initialization
     public Animator animator;
+    private Rigidbody2D playerRigidbody;
+    private CapsuleCollider2D capsuleCollider;
+
     [SerializeField]
     PlayerInventory inventory;
 
+    AUDIO_PlayerMovement audPlayerMovement;
+    #endregion
+
+    #region Player Stats
     [Tooltip("Movement speed of the player")]
-    public float speed = 10;
     [SerializeField]
-    float cameraSpeed = 1.1f;
+    private float speed = 10;
 
     [SerializeField]
     Camera playerCam;
@@ -19,20 +26,22 @@ public class CharacterController2D : MonoBehaviour
     [SerializeField]
     GameObject resetPoint;
 
-    public float jumpHeight = 50;
+    [SerializeField]
+    private float jumpHeight = 50;
 
-    float maxSpeed = 20;
+    [SerializeField]
+    private float maxSpeed = 20;
 
-    new BoxCollider2D collider;
+    //Jumping Bools
+    public bool grounded;
+    private bool canDoubleJump;
+    #endregion
 
-    Rigidbody2D rb;
-
+    #region Environment Data
     public LayerMask groundLayer;
     public Transform groundCheck;
     public float checkRadius;
-    public bool grounded;
-    private bool canDoubleJump;
-    public GameObject player;
+    #endregion
 
     private bool isFacingRight = true;
 
@@ -40,37 +49,31 @@ public class CharacterController2D : MonoBehaviour
     Vector3 cameraOffset = new Vector3(0, 0, -10);
     Vector3 charPos;
     Vector2 input;
-    //This variable is hold the position the Camera will go ahead of the player towards.
-    Vector2 cameraBoost = new Vector2(5f, 5f);
-    
 
     private void Awake()
     {
-        
-        collider = GetComponent<BoxCollider2D>();
-        rb = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
         inventory = GetComponent<PlayerInventory>();
+        audPlayerMovement = GetComponent<AUDIO_PlayerMovement>();
     }
 
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
-        if (rb.velocity.magnitude <= maxSpeed)
+        if (playerRigidbody.velocity.magnitude <= maxSpeed)
         {
-            rb.velocity = new Vector2(input.x * speed, rb.velocity.y);
+            playerRigidbody.velocity = new Vector2(input.x * speed, playerRigidbody.velocity.y);
         }
-    }
-
-    private void Start()
-    {
-
-        playerCam.transform.position = new Vector3(transform.position.x, transform.position.y, playerCam.transform.position.z);
+        grounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        if (playerRigidbody.velocity.magnitude > maxSpeed)
+        {
+            playerRigidbody.velocity = new Vector2(input.x * speed, playerRigidbody.velocity.y);
+        }
     }
 
     private void Update()
     {
-
-
         input.x = Input.GetAxis("Horizontal");
 
         //Updates mouse and character positions
@@ -94,8 +97,9 @@ public class CharacterController2D : MonoBehaviour
         //Jumping from ground
         if (grounded == true && Input.GetKeyDown(KeyCode.Space))
         {
+            audPlayerMovement.jumps();
             canDoubleJump = true;
-            rb.velocity = rb.velocity + Vector2.up * jumpHeight;
+            playerRigidbody.velocity = playerRigidbody.velocity + Vector2.up * jumpHeight;
         }
 
         //Jumping Mid-Air
@@ -104,9 +108,9 @@ public class CharacterController2D : MonoBehaviour
             canDoubleJump = false;
             //"v" saves current X and Y velocity, foribly changes the Y to be a fraction of normal jump height, then sets the RB velocity. 
             //Prevents combining the two jump velocities into a super jump by button mashing.
-            Vector2 v = rb.velocity;
+            Vector2 v = playerRigidbody.velocity;
             v.y = jumpHeight * .8f;
-            rb.velocity = v;
+            playerRigidbody.velocity = v;
         }
 
         if (Input.GetKeyDown(KeyCode.T))
@@ -115,7 +119,7 @@ public class CharacterController2D : MonoBehaviour
         }
         animator.SetFloat("DeltaX", Mathf.Abs(input.x));
 
-        CameraControl();
+        //Testing
     }
 
     //Uses Vector 3 to acquire character position and mouse position 
@@ -132,51 +136,4 @@ public class CharacterController2D : MonoBehaviour
         transform.Rotate(0, 180, 0);
     }
 
-    private void CameraControl()
-    {
-        Vector3 cameraGoal;
-        
-        if (input.x > 0 && isFacingRight)
-        {
-            // I just want to get your love and affection <3
-            // Calculating the distance that the camera will be away from the player.
-            // Based on if the player is moving (Abs(input.x)), the right facing direction of the player (transofrm.right.x), and the distance we want to maintain(cameraboost.x).
-            float distanceToMaintain = Mathf.Abs(input.x) * transform.right.x * cameraBoost.x;
-
-            // Give me cuddles uWu
-            // Adding the distance we want to maintain to the players position
-            // This will calculate the distance the camera should be, relative to the players position
-            float xCameraGoal = distanceToMaintain + transform.position.x;
-
-            // Creating a vector pointing to where the camera should be.
-            // Taking into account the players y position, and the camera Depth.
-            cameraGoal = new Vector3(xCameraGoal, transform.position.y, playerCam.transform.position.z);
-        }
-        else if(input.x < 0 && !isFacingRight)
-        {
-            // I just want to get your love and affection <3
-            // Calculating the distance that the camera will be away from the player.
-            // Based on if the player is moving (Abs(input.x)), the right facing direction of the player (transofrm.right.x), and the distance we want to maintain(cameraboost.x).
-            float distanceToMaintain = Mathf.Abs(input.x) * transform.right.x * cameraBoost.x;
-
-            // Give me cuddles uWu
-            // Adding the distance we want to maintain to the players position
-            // This will calculate the distance the camera should be, relative to the players position
-            float xCameraGoal = distanceToMaintain + transform.position.x;
-
-            // Creating a vector pointing to where the camera should be.
-            // Taking into account the players y position, and the camera Depth.
-            cameraGoal = new Vector3(xCameraGoal, transform.position.y, playerCam.transform.position.z);
-        }
-        else
-            cameraGoal = new Vector3(transform.position.x, transform.position.y, playerCam.transform.position.z);
-
-        StartCoroutine(stayAhead(cameraGoal));
-    }
-
-    IEnumerator stayAhead(Vector3 cameraGoal)
-    {
-        playerCam.transform.position = Vector3.Lerp(playerCam.transform.position, cameraGoal, cameraSpeed);
-        yield return new WaitForSeconds(.5f);
-    }
 }
