7f6f9d65e0bf25b0c4c10d58e3ba5994d958da95
GD278 <57197845+GD278@users.noreply.github.com>
2021-04-08 17:50:25 -0500

    Added Various Sounds

---
+++ Assets/Death_Sound_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Sound_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] AudioClip clip;
+    AudioSource source;
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
+        StartCoroutine(Wait());
+        Debug.Log("Waiting");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(2);
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/Enemy Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/Enemy Scripts/EnemyHealthManager.cs
@@ -7,9 +7,12 @@ public class EnemyHealthManager : MonoBehaviour
     Health health;
     float localHealth;
     Enemy enemy;
-
+    public GameObject EnemyDeathSound;
+    
     private void Die()
     {
+       
+        Instantiate(EnemyDeathSound, this.transform.position, this.transform.rotation);
         StartMenu.score += enemy.scoreValue;
         Destroy(gameObject);
     }
